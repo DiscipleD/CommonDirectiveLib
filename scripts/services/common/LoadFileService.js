c501581a14486602582d08e05eef3c9e394550f7
dingjianzhe <dingjianzhe@quyiyuan.com>
2015-08-28 23:41:00 +0800

    add common service - LoadCSSFile service

---
+++ scripts/services/common/LoadFileService.js
@@ -0,0 +1,23 @@
+/**
+ * Created by qy on 2015/8/28.
+ */
+define([
+    'scripts/services/appServicesModule'
+], function (services) {
+    'use strict';
+    return services.factory('LoadFileService', function(){
+
+        var factory = {};
+        var _basePath = 'localhost:63342/CommonDirectiveLib/'
+
+        factory.loadCssFile = function(path){
+            if (path){
+                var css = $('<link rel="stylesheet"/>');
+                css.attr('href', path);
+                $('head').append(css);
+            }
+        };
+
+        return factory;
+    });
+});
