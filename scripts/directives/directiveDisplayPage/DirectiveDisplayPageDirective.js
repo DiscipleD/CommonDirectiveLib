dd002fad7dddbd7ba6703f4bed63d2eb99a4bc70
Disciple_D <m13917105913@163.com>
2015-09-16 21:08:52 +0800

    version 0.1.1: complete transition on show & hide panel body

--- scripts/directives/directiveDisplayPage/DirectiveDisplayPageDirective.js
+++ scripts/directives/directiveDisplayPage/DirectiveDisplayPageDirective.js
@@ -6,7 +6,7 @@
 define([
     'scripts/directives/appDirectivesModule'
 ], function(directives){
-    directives.directive('cdlDirectiveDisplayPage', ['$compile', function($compile){
+    directives.directive('cdlDirectiveDisplayPage', ['$compile', '$timeout', function($compile, $timeout){
         return {
             restrict: 'A',
             templateUrl: 'views/directiveDisplayPage/directiveDisplayPage.html',
@@ -17,8 +17,30 @@ define([
 
                 element.find('.panel-heading').on('click', function () {
                     angular.element(this).toggleClass('expanded');
-                    angular.element(this).parent().find('.panel-body').toggleClass('collapse');
+                    var panelElement = angular.element(this).parent().find('.panel-body').parent();
+                    scope.toggleCollapsePanel(panelElement);
                 });
+
+                // set 'collapse-body' element height for transition animation
+                $timeout(function () {
+                    element.find('.collapse-body:not(.collapsed)').each(function () {
+                        $(this).css('height', $(this).find('.panel-body').css('height'));
+                    });
+                    element.find('.collapse-body.collapsed').each(function () {
+                        $(this).css('height', 0);
+                    });
+                }, 500);
+
+                scope.toggleCollapsePanel = function (panelElement) {
+                    if (panelElement.hasClass('collapsed')){
+                        panelElement.removeClass('collapsed');
+
+                        panelElement.css('height', panelElement.find('.panel-body').css('height'));
+                    }else{
+                        panelElement.addClass('collapsed');
+                        panelElement.css('height', 0);
+                    }
+                }
             }
         };
     }]);
